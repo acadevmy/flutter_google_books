2b53c73c44d293b4f71b740a14071ac9daaa6797
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2024-01-19 17:15:48 +0100

    feat: replace StatefulWidget with HookWidget

--- lib/presentation/category/category_screen.dart
+++ lib/presentation/category/category_screen.dart
@@ -3,82 +3,74 @@ import 'package:flutter_google_books/application/repositories/i_volume_repositor
 import 'package:flutter_google_books/injection.dart';
 import 'package:flutter_google_books/presentation/category/widgets/category_list_view.dart';
 import 'package:flutter_google_books/presentation/volume/widgets/volume_list_view.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class CategoryScreen extends StatefulWidget {
+class CategoryScreen extends HookWidget {
   const CategoryScreen({super.key});
 
   @override
-  State<CategoryScreen> createState() => _CategoryScreenState();
-}
-
-class _CategoryScreenState extends State<CategoryScreen> {
-  var title = '';
-  var controller = TextEditingController(text: '');
-
-  @override
-  void dispose() {
-    super.dispose();
-    controller.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    controller.addListener(() {
-      setState(() => title = controller.text);
-    });
+    final controller = useTextEditingController(text: '');
+    useListenable(controller);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Google Books'),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
-          child: Column(
-            children: [
-              Form(
-                child: TextFormField(
-                  controller: controller,
-                  decoration: InputDecoration(
-                    suffixIcon: Visibility(
-                      visible: controller.text.isNotEmpty,
-                      child: IconButton(
-                        onPressed: () => controller.text = '',
-                        icon: const Icon(Icons.clear),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Google Books'),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+            child: Column(
+              children: [
+                Form(
+                  child: TextFormField(
+                    controller: controller,
+                    decoration: InputDecoration(
+                      suffixIcon: Visibility(
+                        visible: controller.text.isNotEmpty,
+                        child: IconButton(
+                          onPressed: () {
+                            controller.text = '';
+                            FocusScope.of(context).unfocus();
+                          },
+                          icon: const Icon(Icons.clear),
+                        ),
                       ),
-                    ),
-                    floatingLabelBehavior: FloatingLabelBehavior.never,
-                    contentPadding: const EdgeInsets.all(4),
-                    prefixIcon: const Icon(Icons.search),
-                    labelText: 'Search...',
-                    border: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      floatingLabelBehavior: FloatingLabelBehavior.never,
+                      contentPadding: const EdgeInsets.all(4),
+                      prefixIcon: const Icon(Icons.search),
+                      labelText: 'Search...',
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 20.0),
-              Builder(
-                builder: (context) {
-                  if (title.isEmpty) {
-                    return CategoryListView(
-                      onTap: (category) => Navigator.pushNamed(context, '/volume', arguments: category),
+                const SizedBox(height: 20.0),
+                Builder(
+                  builder: (context) {
+                    if (controller.text.isEmpty) {
+                      return CategoryListView(
+                        onTap: (category) => Navigator.pushNamed(context, '/volume', arguments: category),
+                      );
+                    }
+
+                    return FutureBuilder(
+                      future: getIt<IVolumeRepository>().searchBy(controller.text),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return VolumeListView(volumes: snapshot.requireData);
+                        }
+
+                        return const Center(child: CircularProgressIndicator());
+                      },
                     );
-                  }
-
-                  return FutureBuilder(
-                    future: getIt<IVolumeRepository>().searchBy(title),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        return VolumeListView(volumes: snapshot.requireData);
-                      }
-
-                      return const Center(child: CircularProgressIndicator());
-                    },
-                  );
-                },
-              ),
-            ],
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
