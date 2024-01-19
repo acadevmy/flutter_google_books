d72a73c5fa40396d611d038b8c7290ce53c1bc40
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2024-01-19 15:17:57 +0100

    feat: add list tile to display categories

--- lib/presentation/category/category_screen.dart
+++ lib/presentation/category/category_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_google_books/presentation/category/widgets/category_list_tile.dart';
 
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({super.key});
@@ -9,17 +10,13 @@ class CategoryScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter Google Books'),
       ),
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text('You have created your second screen:'),
-              Text(
-                'Category',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
+      body: const SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+          child: CategoryListTile(
+            icon: Icons.soup_kitchen,
+            title: 'La cucina per tutti di Casa Pappagallo',
+            subtitle: 'Un ricettario che celebra il cibo e la condivisione',
           ),
         ),
       ),
