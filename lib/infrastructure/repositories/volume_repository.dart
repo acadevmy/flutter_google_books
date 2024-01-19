3bfa9956b09c8b59ad248e52d1edbf84555a3cea
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2024-01-19 15:12:07 +0100

    feat: add volume repository implementation

---
+++ lib/infrastructure/repositories/volume_repository.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:flutter_google_books/application/repositories/i_volume_repository.dart';
+import 'package:flutter_google_books/domain/entities/volume.dart';
+import 'package:flutter_google_books/infrastructure/data_transfer_objects/volume_response_dto.dart';
+import 'package:http/http.dart' as http;
+import 'package:injectable/injectable.dart';
+
+@LazySingleton(as: IVolumeRepository)
+class VolumeRepository implements IVolumeRepository {
+  final _baseUrl = 'https://www.googleapis.com';
+
+  @override
+  Future<List<Volume>> searchBy(String title) {
+    // TODO: implement searchBy
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Volume>> getByCategory(String category) async {
+    final response = await http.get(Uri.parse('$_baseUrl/books/v1/volumes?q=subject:$category'));
+    final decoded = jsonDecode(utf8.decode(response.bodyBytes)) as Map<String, dynamic>;
+    final items = VolumeResponseDto.fromJson(decoded).items;
+
+    return items.map((item) => item.toDomain()).toList();
+  }
+
+  @override
+  Future<Volume> getById(String id) {
+    // TODO: implement getById
+    throw UnimplementedError();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_google_books/injection.dart';
 import 'package:flutter_google_books/presentation/category/category_screen.dart';
 import 'package:flutter_google_books/presentation/home/home_screen.dart';
 import 'package:json_theme/json_theme.dart';
 
 void main() async {
+  configureDependencies();
   WidgetsFlutterBinding.ensureInitialized();
 
   final json = await rootBundle.loadString('assets/appainter_theme.json');
