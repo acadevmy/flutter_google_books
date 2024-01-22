aa5b26cf161d4ed50da0483b0762e36c2341426f
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2024-01-22 18:00:15 +0100

    feat: show in FAB the number of items added as favorite

--- lib/presentation/widgets/favorite_fab.dart
+++ lib/presentation/widgets/favorite_fab.dart
@@ -1,4 +1,7 @@
+import 'package:badges/badges.dart' as badges;
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_google_books/application/volume_details/volume_details_cubit.dart';
 
 class FavoriteFab extends StatelessWidget {
   final VoidCallback onPressed;
@@ -13,7 +16,13 @@ class FavoriteFab extends StatelessWidget {
     return FloatingActionButton(
       onPressed: onPressed,
       shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(100.0))),
-      child: const Icon(Icons.list_outlined),
+      child: badges.Badge(
+        badgeStyle: badges.BadgeStyle(badgeColor: Theme.of(context).primaryColor),
+        badgeContent: BlocBuilder<VolumeDetailsCubit, VolumeDetailsState>(
+          builder: (context, state) => Text(state.favorites.length.toString()),
+        ),
+        child: const Icon(Icons.list_outlined),
+      ),
     );
   }
 }
