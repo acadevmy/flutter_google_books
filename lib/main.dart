afe7211528f3a4cd633574808cd60e976c5373e9
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2024-02-29 21:15:49 +0100

    build: install Firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_google_books/application/volume_details/volume_details_cubit.dart';
+import 'package:flutter_google_books/firebase_options.dart';
 import 'package:flutter_google_books/injection.dart';
 import 'package:flutter_google_books/router.dart';
 import 'package:json_theme/json_theme.dart';
@@ -11,6 +13,9 @@ import 'package:json_theme/json_theme.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await configureDependencies();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   final json = await rootBundle.loadString('assets/appainter_theme.json');
   final theme = ThemeDecoder.decodeThemeData(jsonDecode(json))!;
