fbc6760057b90cc5dc44e0bf88c5adc42858bffc
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2024-02-29 21:15:56 +0100

    feat: add auth form widget

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_google_books/presentation/home/widgets/auth_form.dart';
 import 'package:flutter_google_books/router.dart';
 
 @RoutePage()
@@ -14,33 +15,40 @@ class HomeScreen extends StatelessWidget {
       ),
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                width: 300.0,
-                child: Text(
-                  'Flutter Google Books',
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  width: 300.0,
+                  child: Text(
+                    'Flutter Google Books',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20.0),
-              const Image(
-                image: AssetImage('assets/images/bibliophile.png'),
-                width: 350.0,
-              ),
-              const SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () => context.replaceRoute(const CategoryOverviewRoute()),
-                style: ButtonStyle(
-                  minimumSize: MaterialStateProperty.all(const Size(250.0, 50.0)),
+                const SizedBox(height: 20.0),
+                const Image(
+                  image: AssetImage('assets/images/bibliophile.png'),
+                  width: 300.0,
                 ),
-                child: const Text('Show Categories'),
-              ),
-            ],
+                const SizedBox(height: 20.0),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40.0),
+                  child: AuthForm(),
+                ),
+                const SizedBox(height: 30.0),
+                ElevatedButton(
+                  onPressed: () => context.replaceRoute(const CategoryOverviewRoute()),
+                  style: ButtonStyle(
+                    minimumSize: MaterialStateProperty.all(const Size(250.0, 50.0)),
+                  ),
+                  child: const Text('Show Categories'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
